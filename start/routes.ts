303ab579d48e6744f62bc7cee6de7d1a951df031
Naguib <moma.naguib@gmail.com>
2024-11-16 23:18:56 +0100

    feat: add routes auth

--- start/routes.ts
+++ start/routes.ts
@@ -8,6 +8,17 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+
+router
+  .group(() => {
+    router.post('/register', 'AuthController.register')
+    router.post('/login', 'AuthController.login')
+    router.post('/logout', 'AuthController.logout').use(middleware.auth())
+  })
+  .prefix('/auth')
+
+router.get('/me', 'AuthController.me').use(middleware.auth())
 
 router.get('/', async () => {
   return {
