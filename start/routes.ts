ab1a186dd5a462a5134ed0a81788bafc52b468e3
Naguib <moma.naguib@gmail.com>
2024-11-16 23:43:51 +0100

    feat(routes): add endpoints for contract CRUD

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,17 @@ router
 
 router.get('/me', 'AuthController.me').use(middleware.auth())
 
+router
+  .group(() => {
+    router.get('/', 'ContractController.index')
+    router.post('/', 'ContractController.store')
+    router.get('/:id', 'ContractController.show')
+    router.put('/:id', 'ContractController.update')
+    router.delete('/:id', 'ContractController.destroy')
+  })
+  .prefix('/contracts')
+  .use(middleware.auth())
+
 router.get('/', async () => {
   return {
     hello: 'world',
